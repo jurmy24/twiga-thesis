840bcab29f1d5861034129da8387d814df47d981
Victor Oldensand <victormag24@gmail.com>
2024-03-26 13:51:23 +0300

    Misc: test run of script to parse pdf

--- script.py
+++ script.py
@@ -1,6 +1,23 @@
-from src import KnowledgeParser, modules, QuestionGenerator, utils
+import os
+from dotenv import load_dotenv
 
+from src.KnowledgeParser import KnowledgeParser
+
+load_dotenv()
+LLAMA_CLOUD_API_KEY = os.getenv("LLAMA_CLOUD_API_KEY")
+
+# Base directory where the output PDFs will be saved
+DATA_DIR = os.getenv('DATA_DIR_PATH')
 
 """
 This file is for running a few scripts and testing things.
 """
+
+if __name__ == "__main__":
+
+    # Initialize the KnowledgeParser
+    knowledge_parser = KnowledgeParser(api_key=LLAMA_CLOUD_API_KEY)
+
+    # Extract the relevant pages
+    new_pdf_path = knowledge_parser.extract_pages(pdf_path=f"{DATA_DIR}airbnb-faq.pdf", start_page=0, end_page=None)
+    # documents = knowledge_parser.parse_pdf_sync(new_pdf_path)

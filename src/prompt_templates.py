40c5bf88b7060628d1d24733d6f5f9efb08cf0ae
Victor Oldensand <victormag24@gmail.com>
2024-04-02 11:43:53 +0200

    Feat: create prompt template file

---
+++ src/prompt_templates.py
@@ -0,0 +1,196 @@
+
+"""
+
+Directly from LlamaIndex (llama_index/llama-index-core/llama_index/core/prompts/default_prompts.py)
+
+"""
+
+from llama_index.core.prompts.base import PromptTemplate
+from llama_index.core.prompts.prompt_type import PromptType
+
+DEFAULT_TEXT_QA_PROMPT_TMPL = (
+    "Context information is below.\n"
+    "---------------------\n"
+    "{context_str}\n"
+    "---------------------\n"
+    "Given the context information and not prior knowledge, "
+    "answer the query.\n"
+    "Query: {query_str}\n"
+    "Answer: "
+)
+
+DEFAULT_TEXT_QA_PROMPT = PromptTemplate(
+    DEFAULT_TEXT_QA_PROMPT_TMPL, prompt_type=PromptType.QUESTION_ANSWER
+)
+
+DEFAULT_TREE_SUMMARIZE_TMPL = (
+    "Context information from multiple sources is below.\n"
+    "---------------------\n"
+    "{context_str}\n"
+    "---------------------\n"
+    "Given the information from multiple sources and not prior knowledge, "
+    "answer the query.\n"
+    "Query: {query_str}\n"
+    "Answer: "
+)
+DEFAULT_TREE_SUMMARIZE_PROMPT = PromptTemplate(
+    DEFAULT_TREE_SUMMARIZE_TMPL, prompt_type=PromptType.SUMMARY
+)
+
+# NOTE: by partially filling schema, we can reduce to a QuestionAnswer prompt
+# that we can feed to ur table
+DEFAULT_TABLE_CONTEXT_TMPL = (
+    "We have provided a table schema below. "
+    "---------------------\n"
+    "{schema}\n"
+    "---------------------\n"
+    "We have also provided context information below. "
+    "{context_str}\n"
+    "---------------------\n"
+    "Given the context information and the table schema, "
+    "give a response to the following task: {query_str}"
+)
+
+DEFAULT_TABLE_CONTEXT_QUERY = (
+    "Provide a high-level description of the table, "
+    "as well as a description of each column in the table. "
+    "Provide answers in the following format:\n"
+    "TableDescription: <description>\n"
+    "Column1Description: <description>\n"
+    "Column2Description: <description>\n"
+    "...\n\n"
+)
+
+DEFAULT_TABLE_CONTEXT_PROMPT = PromptTemplate(
+    DEFAULT_TABLE_CONTEXT_TMPL, prompt_type=PromptType.TABLE_CONTEXT
+)
+
+# NOTE: by partially filling schema, we can reduce to a refine prompt
+# that we can feed to ur table
+DEFAULT_REFINE_TABLE_CONTEXT_TMPL = (
+    "We have provided a table schema below. "
+    "---------------------\n"
+    "{schema}\n"
+    "---------------------\n"
+    "We have also provided some context information below. "
+    "{context_msg}\n"
+    "---------------------\n"
+    "Given the context information and the table schema, "
+    "give a response to the following task: {query_str}\n"
+    "We have provided an existing answer: {existing_answer}\n"
+    "Given the new context, refine the original answer to better "
+    "answer the question. "
+    "If the context isn't useful, return the original answer."
+)
+DEFAULT_REFINE_TABLE_CONTEXT_PROMPT = PromptTemplate(
+    DEFAULT_REFINE_TABLE_CONTEXT_TMPL, prompt_type=PromptType.TABLE_CONTEXT
+)
+
+# Here is an example using two-shot prompting (i.e. providing examples)
+DEFAULT_KG_TRIPLET_EXTRACT_TMPL = (
+    "Some text is provided below. Given the text, extract up to "
+    "{max_knowledge_triplets} "
+    "knowledge triplets in the form of (subject, predicate, object). Avoid stopwords.\n"
+    "---------------------\n"
+    "Example:"
+    "Text: Alice is Bob's mother."
+    "Triplets:\n(Alice, is mother of, Bob)\n"
+    "Text: Philz is a coffee shop founded in Berkeley in 1982.\n"
+    "Triplets:\n"
+    "(Philz, is, coffee shop)\n"
+    "(Philz, founded in, Berkeley)\n"
+    "(Philz, founded in, 1982)\n"
+    "---------------------\n"
+    "Text: {text}\n"
+    "Triplets:\n"
+)
+DEFAULT_KG_TRIPLET_EXTRACT_PROMPT = PromptTemplate(
+    DEFAULT_KG_TRIPLET_EXTRACT_TMPL,
+    prompt_type=PromptType.KNOWLEDGE_TRIPLET_EXTRACT,
+)
+
+
+"""
+
+Directly from LlamaIndex (llama_index/llama-index-core/llama_index/core/prompts/chat_prompts.py)
+
+"""
+
+from llama_index.core.base.llms.types import ChatMessage, MessageRole
+from llama_index.core.prompts.base import ChatPromptTemplate
+
+# text qa prompt
+TEXT_QA_SYSTEM_PROMPT = ChatMessage(
+    content=(
+        "You are an expert Q&A system that is trusted around the world.\n"
+        "Always answer the query using the provided context information, "
+        "and not prior knowledge.\n"
+        "Some rules to follow:\n"
+        "1. Never directly reference the given context in your answer.\n"
+        "2. Avoid statements like 'Based on the context, ...' or "
+        "'The context information ...' or anything along "
+        "those lines."
+    ),
+    role=MessageRole.SYSTEM,
+)
+
+TEXT_QA_PROMPT_TMPL_MSGS = [
+    TEXT_QA_SYSTEM_PROMPT,
+    ChatMessage(
+        content=(
+            "Context information is below.\n"
+            "---------------------\n"
+            "{context_str}\n"
+            "---------------------\n"
+            "Given the context information and not prior knowledge, "
+            "answer the query.\n"
+            "Query: {query_str}\n"
+            "Answer: "
+        ),
+        role=MessageRole.USER,
+    ),
+]
+
+CHAT_TEXT_QA_PROMPT = ChatPromptTemplate(message_templates=TEXT_QA_PROMPT_TMPL_MSGS)
+
+# Tree Summarize
+TREE_SUMMARIZE_PROMPT_TMPL_MSGS = [
+    TEXT_QA_SYSTEM_PROMPT,
+    ChatMessage(
+        content=(
+            "Context information from multiple sources is below.\n"
+            "---------------------\n"
+            "{context_str}\n"
+            "---------------------\n"
+            "Given the information from multiple sources and not prior knowledge, "
+            "answer the query.\n"
+            "Query: {query_str}\n"
+            "Answer: "
+        ),
+        role=MessageRole.USER,
+    ),
+]
+
+CHAT_TREE_SUMMARIZE_PROMPT = ChatPromptTemplate(
+    message_templates=TREE_SUMMARIZE_PROMPT_TMPL_MSGS
+)
+
+
+# Refine Prompt (a variant of this one can be used for the query rewriter)
+CHAT_REFINE_PROMPT_TMPL_MSGS = [
+    ChatMessage(
+        content=(
+            "You are an expert Q&A system that strictly operates in two modes "
+            "when refining existing answers:\n"
+            "1. **Rewrite** an original answer using the new context.\n"
+            "2. **Repeat** the original answer if the new context isn't useful.\n"
+            "Never reference the original answer or context directly in your answer.\n"
+            "When in doubt, just repeat the original answer.\n"
+            "New Context: {context_msg}\n"
+            "Query: {query_str}\n"
+            "Original Answer: {existing_answer}\n"
+            "New Answer: "
+        ),
+        role=MessageRole.USER,
+    )
+]

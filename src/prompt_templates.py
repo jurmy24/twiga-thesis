02868728322fa73c8a73e352c0c6c60b57a60621
Victor Oldensand <victormag24@gmail.com>
2024-04-02 13:00:44 +0200

    Misc: remove LlamaIndex dependency in the prompt templates

--- src/prompt_templates.py
+++ src/prompt_templates.py
@@ -1,14 +1,9 @@
+from src.models import ChatMessage
 
 """
-
-Directly from LlamaIndex (llama_index/llama-index-core/llama_index/core/prompts/default_prompts.py)
-
+From LlamaIndex (llama_index/llama-index-core/llama_index/core/prompts/default_prompts.py)
 """
-
-from llama_index.core.prompts.base import PromptTemplate
-from llama_index.core.prompts.prompt_type import PromptType
-
-DEFAULT_TEXT_QA_PROMPT_TMPL = (
+DEFAULT_TEXT_QA_PROMPT = (
     "Context information is below.\n"
     "---------------------\n"
     "{context_str}\n"
@@ -19,11 +14,7 @@ DEFAULT_TEXT_QA_PROMPT_TMPL = (
     "Answer: "
 )
 
-DEFAULT_TEXT_QA_PROMPT = PromptTemplate(
-    DEFAULT_TEXT_QA_PROMPT_TMPL, prompt_type=PromptType.QUESTION_ANSWER
-)
-
-DEFAULT_TREE_SUMMARIZE_TMPL = (
+DEFAULT_TREE_SUMMARIZE = (
     "Context information from multiple sources is below.\n"
     "---------------------\n"
     "{context_str}\n"
@@ -33,13 +24,10 @@ DEFAULT_TREE_SUMMARIZE_TMPL = (
     "Query: {query_str}\n"
     "Answer: "
 )
-DEFAULT_TREE_SUMMARIZE_PROMPT = PromptTemplate(
-    DEFAULT_TREE_SUMMARIZE_TMPL, prompt_type=PromptType.SUMMARY
-)
 
 # NOTE: by partially filling schema, we can reduce to a QuestionAnswer prompt
 # that we can feed to ur table
-DEFAULT_TABLE_CONTEXT_TMPL = (
+DEFAULT_TABLE_CONTEXT_PROMPT = (
     "We have provided a table schema below. "
     "---------------------\n"
     "{schema}\n"
@@ -51,43 +39,8 @@ DEFAULT_TABLE_CONTEXT_TMPL = (
     "give a response to the following task: {query_str}"
 )
 
-DEFAULT_TABLE_CONTEXT_QUERY = (
-    "Provide a high-level description of the table, "
-    "as well as a description of each column in the table. "
-    "Provide answers in the following format:\n"
-    "TableDescription: <description>\n"
-    "Column1Description: <description>\n"
-    "Column2Description: <description>\n"
-    "...\n\n"
-)
-
-DEFAULT_TABLE_CONTEXT_PROMPT = PromptTemplate(
-    DEFAULT_TABLE_CONTEXT_TMPL, prompt_type=PromptType.TABLE_CONTEXT
-)
-
-# NOTE: by partially filling schema, we can reduce to a refine prompt
-# that we can feed to ur table
-DEFAULT_REFINE_TABLE_CONTEXT_TMPL = (
-    "We have provided a table schema below. "
-    "---------------------\n"
-    "{schema}\n"
-    "---------------------\n"
-    "We have also provided some context information below. "
-    "{context_msg}\n"
-    "---------------------\n"
-    "Given the context information and the table schema, "
-    "give a response to the following task: {query_str}\n"
-    "We have provided an existing answer: {existing_answer}\n"
-    "Given the new context, refine the original answer to better "
-    "answer the question. "
-    "If the context isn't useful, return the original answer."
-)
-DEFAULT_REFINE_TABLE_CONTEXT_PROMPT = PromptTemplate(
-    DEFAULT_REFINE_TABLE_CONTEXT_TMPL, prompt_type=PromptType.TABLE_CONTEXT
-)
-
 # Here is an example using two-shot prompting (i.e. providing examples)
-DEFAULT_KG_TRIPLET_EXTRACT_TMPL = (
+DEFAULT_KG_TRIPLET_EXTRACT_PROMPT = (
     "Some text is provided below. Given the text, extract up to "
     "{max_knowledge_triplets} "
     "knowledge triplets in the form of (subject, predicate, object). Avoid stopwords.\n"
@@ -104,23 +57,16 @@ DEFAULT_KG_TRIPLET_EXTRACT_TMPL = (
     "Text: {text}\n"
     "Triplets:\n"
 )
-DEFAULT_KG_TRIPLET_EXTRACT_PROMPT = PromptTemplate(
-    DEFAULT_KG_TRIPLET_EXTRACT_TMPL,
-    prompt_type=PromptType.KNOWLEDGE_TRIPLET_EXTRACT,
-)
 
 
 """
-
 Directly from LlamaIndex (llama_index/llama-index-core/llama_index/core/prompts/chat_prompts.py)
-
 """
 
-from llama_index.core.base.llms.types import ChatMessage, MessageRole
-from llama_index.core.prompts.base import ChatPromptTemplate
+# TODO: determine whether to use ChatMessage here or to just use the prompt literal like what is done above
 
 # text qa prompt
-TEXT_QA_SYSTEM_PROMPT = ChatMessage(
+CHAT_TEXT_QA_SYSTEM_PROMPT = ChatMessage(
     content=(
         "You are an expert Q&A system that is trusted around the world.\n"
         "Always answer the query using the provided context information, "
@@ -131,68 +77,52 @@ TEXT_QA_SYSTEM_PROMPT = ChatMessage(
         "'The context information ...' or anything along "
         "those lines."
     ),
-    role=MessageRole.SYSTEM,
+    role="system",
 )
 
-TEXT_QA_PROMPT_TMPL_MSGS = [
-    TEXT_QA_SYSTEM_PROMPT,
-    ChatMessage(
-        content=(
-            "Context information is below.\n"
-            "---------------------\n"
-            "{context_str}\n"
-            "---------------------\n"
-            "Given the context information and not prior knowledge, "
-            "answer the query.\n"
-            "Query: {query_str}\n"
-            "Answer: "
-        ),
-        role=MessageRole.USER,
+CHAT_TEXT_QA_USER_PROMPT = ChatMessage(
+    content=(
+        "Context information is below.\n"
+        "---------------------\n"
+        "{context_str}\n"
+        "---------------------\n"
+        "Given the context information and not prior knowledge, "
+        "answer the query.\n"
+        "Query: {query_str}\n"
+        "Answer: "
     ),
-]
-
-CHAT_TEXT_QA_PROMPT = ChatPromptTemplate(message_templates=TEXT_QA_PROMPT_TMPL_MSGS)
+    role="user"
+)
 
 # Tree Summarize
-TREE_SUMMARIZE_PROMPT_TMPL_MSGS = [
-    TEXT_QA_SYSTEM_PROMPT,
-    ChatMessage(
-        content=(
-            "Context information from multiple sources is below.\n"
-            "---------------------\n"
-            "{context_str}\n"
-            "---------------------\n"
-            "Given the information from multiple sources and not prior knowledge, "
-            "answer the query.\n"
-            "Query: {query_str}\n"
-            "Answer: "
-        ),
-        role=MessageRole.USER,
+CHAT_TREE_SUMMARIZE_USER_PROMPT = ChatMessage(
+    content=(
+        "Context information from multiple sources is below.\n"
+        "---------------------\n"
+        "{context_str}\n"
+        "---------------------\n"
+        "Given the information from multiple sources and not prior knowledge, "
+        "answer the query.\n"
+        "Query: {query_str}\n"
+        "Answer: "
     ),
-]
-
-CHAT_TREE_SUMMARIZE_PROMPT = ChatPromptTemplate(
-    message_templates=TREE_SUMMARIZE_PROMPT_TMPL_MSGS
-)
-
+    role="user",
+    )
 
 # Refine Prompt (a variant of this one can be used for the query rewriter)
-CHAT_REFINE_PROMPT_TMPL_MSGS = [
-    ChatMessage(
-        content=(
-            "You are an expert Q&A system that strictly operates in two modes "
-            "when refining existing answers:\n"
-            "1. **Rewrite** an original answer using the new context.\n"
-            "2. **Repeat** the original answer if the new context isn't useful.\n"
-            "Never reference the original answer or context directly in your answer.\n"
-            "When in doubt, just repeat the original answer.\n"
-            "New Context: {context_msg}\n"
-            "Query: {query_str}\n"
-            "Original Answer: {existing_answer}\n"
-            "New Answer: "
-        ),
-        role=MessageRole.USER,
+CHAT_REFINE_USER_PROMPT = ChatMessage(
+    content=(
+        "You are an expert Q&A system that strictly operates in two modes "
+        "when refining existing answers:\n"
+        "1. **Rewrite** an original answer using the new context.\n"
+        "2. **Repeat** the original answer if the new context isn't useful.\n"
+        "Never reference the original answer or context directly in your answer.\n"
+        "When in doubt, just repeat the original answer.\n"
+        "New Context: {context_msg}\n"
+        "Query: {query_str}\n"
+        "Original Answer: {existing_answer}\n"
+        "New Answer: "
+    ),
+    role="user",
     )
-]
 
-CHAT_REFINE_PROMPT = ChatPromptTemplate(message_templates=CHAT_REFINE_PROMPT_TMPL_MSGS)
--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ import json
 from typing import List
 from llama_index.core.schema import Document
 import os
-from src.prompt_templates import DEFAULT_TEXT_QA_PROMPT, DEFAULT_KG_TRIPLET_EXTRACT_PROMPT, CHAT_TEXT_QA_PROMPT, CHAT_REFINE_PROMPT
+from src.prompt_templates import DEFAULT_TEXT_QA_PROMPT, DEFAULT_KG_TRIPLET_EXTRACT_PROMPT, CHAT_TEXT_QA_SYSTEM_PROMPT, CHAT_TEXT_QA_USER_PROMPT
 from src.models import ChatMessage
 
 def save_documents_as_json(documents: List[Document], output_path: str):
@@ -102,38 +102,13 @@ def save_objects_as_json(objects, filename="objects.json"):
         json.dump(objects_data, file, ensure_ascii=False, indent=4)
 
 def generate_text_qa_prompt(context: str, query: str) -> str:
-    # Can be called in the following way:
-    # context = "The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France."
-    # query = "What is the Eiffel Tower made of?"
-
-    # text_qa_prompt = generate_text_qa_prompt(context, query)
-
-    prompt = DEFAULT_TEXT_QA_PROMPT.template.format(context_str=context, query_str=query)
+    prompt = DEFAULT_TEXT_QA_PROMPT.format(context_str=context, query_str=query)
     return prompt
 
 def generate_kg_triplet_prompt(text: str, max_triplets:int=3) -> str:
-    prompt = DEFAULT_KG_TRIPLET_EXTRACT_PROMPT.template.format(text=text, max_knowledge_triplets=max_triplets)
+    prompt = DEFAULT_KG_TRIPLET_EXTRACT_PROMPT.format(text=text, max_knowledge_triplets=max_triplets)
     return prompt
 
-def generate_chat_text_qa_prompt(context: str, query: str) -> List[ChatMessage]:
-    # Prepare the context and query message
-    context_query_msg = ChatMessage(
-        content=f"Context information is below.\n---------------------\n{context}\n---------------------\nGiven the context information and not prior knowledge, answer the query.\nQuery: {query}\nAnswer: ",
-        role="user",
-    )
-
-    # Combine system prompt with the user's context and query
-    prompt_messages = [CHAT_TEXT_QA_PROMPT.message_templates[0], context_query_msg]
-    return prompt_messages
-
-
-def generate_chat_refine_prompt(context: str, query: str, existing_answer: str) -> List[ChatMessage]:
-    # Create a new context message with the task of refining an existing answer
-    refine_msg = ChatMessage(
-        content=f"New Context: {context}\nQuery: {query}\nOriginal Answer: {existing_answer}\nNew Answer: ",
-        role="user",
-    )
-
-    # The first message is a generic instruction for refinement tasks
-    prompt_messages = [CHAT_REFINE_PROMPT.message_templates[0], refine_msg]
-    return prompt_messages
+def generate_chat_text_qa_user_prompt(context: str, query: str) -> ChatMessage:
+    prompt = CHAT_TEXT_QA_USER_PROMPT.content.format(context_str=context, query_str=query)
+    return ChatMessage(content=prompt, role="user")

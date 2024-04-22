983f17ceb34fbb8cd3c9f16b2e75989af887c1b7
Victor Oldensand <victormag24@gmail.com>
2024-04-22 21:38:54 +0200

    Misc: start creating embedder to send in knowledge source to Elastic Search

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,7 +4,7 @@ import os
 import json
 from pprint import pprint
 import time
-from src.models import DocumentSchema
+from src.models import ChunkSchema
 from datetime import datetime
 from typing import List
 
@@ -18,26 +18,30 @@ class DataLoader:
     def __init__(self):
         self.es = Elasticsearch(cloud_id=ELASTIC_CLOUD_ID, api_key=ELASTIC_SEARCH_API_KEY)
         client_info = self.es.info()
+        # self.model = TBD
         print('Connected to Elasticsearch!')
         pprint(client_info.body)
+
+    def get_embedding(self, text):
+        return self.model.encode(text)
 
     def create_index(self):
         self.es.indices.delete(index='twiga_documents', ignore_unavailable=True)
         self.es.indices.create(index='twiga_documents')
 
-    def insert_document(self, document: DocumentSchema):
+    def insert_document(self, document: ChunkSchema):
         return self.es.index(
             index='twiga_documents', 
             body=document.model_dump_json()
         )
     
-    def insert_documents(self, documents: List[DocumentSchema]):
+    def insert_documents(self, documents: List[ChunkSchema]):
         operations = []
         for document in documents:
             operations.append({'index': {'_index': 'twiga_documents'}})
             operations.append(document.model_dump_json())
 
-        return self.es.bulk(operations=operations)
+        return self.es.bulk(operations=operations, pipeline="ent-search-generic-ingestion")
     
     def reindex(self):
         # This method is used if I want to regenerate the index 
@@ -45,7 +49,7 @@ class DataLoader:
         with open(os.path.join(DATA_DIR, "documents", "json", "PLACEHOLDER.json"), 'rt') as f:
             data = json.loads(f.read())
             # Convert JSON objects to DocumentSchema instances
-            documents: List[DocumentSchema] = [DocumentSchema(**doc) for doc in data]
+            documents: List[ChunkSchema] = [ChunkSchema(**doc) for doc in data]
         return self.insert_documents(documents)
     
     def search(self, **query_args):
@@ -57,26 +61,28 @@ class DataLoader:
 
 if __name__ == "__main__":
 
-    dataLoader = DataLoader()
-    document = DocumentSchema(
-        title='Work From Home Policy',
-        contents='The purpose of this full-time work-from-home policy is...',
-        created_on=datetime.now().isoformat()
-    )
+    data_loader = DataLoader()
+    data_loader.create_index()
 
-    # response = dataLoader.insert_document(document)
-    # print(response['_id'])
+    # document = ChunkSchema(
+    #     title='Work From Home Policy',
+    #     contents='The purpose of this full-time work-from-home policy is...',
+    #     created_on=datetime.now().isoformat()
+    # )
 
-    # This searches based on the Okapi BM25 algorithm (a higher score indicates a closer match to the query text)
-    results = dataLoader.search(
-        query={
-            'match': {
-                'name': {
-                    'query': "some text that I search on to match the name of a document"
-                }
-            }
-        }
-    )
+    # # response = dataLoader.insert_document(document)
+    # # print(response['_id'])
+
+    # # This searches based on the Okapi BM25 algorithm (a higher score indicates a closer match to the query text)
+    # results = dataLoader.search(
+    #     query={
+    #         'match': {
+    #             'name': {
+    #                 'query': "some text that I search on to match the name of a document"
+    #             }
+    #         }
+    #     }
+    # )
     
-    matches = results['hits']['hits']
-    total = results['hits']['total']['value']
+    # matches = results['hits']['hits']
+    # total = results['hits']['total']['value']

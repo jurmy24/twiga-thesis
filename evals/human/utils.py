3bdac548dc0a801ed17f389fccbcffb90f4d05bf
Victor Oldensand <victormag24@gmail.com>
2024-05-08 18:22:50 +0200

    Feat: write human eval datasets for multi dimension grading

---
+++ evals/human/utils.py
@@ -0,0 +1,42 @@
+import json
+
+def read_json(file_path):
+    """Utility to read a JSON file."""
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+def write_json(data, file_path):
+    """Utility to write data to a JSON file."""
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
+
+def find_responses(first_file, second_file, output_file):
+    # Load data from both JSON files
+    exercises = read_json(first_file)
+    detailed_responses = read_json(second_file)
+    
+    # Flatten the exercise queries into a list with added type info
+    flattened_queries = []
+    for exercise_type, queries in exercises.items():
+        for query in queries:
+            query['exercise_type'] = exercise_type  # Add type to ease matching
+            flattened_queries.append(query)
+    
+    # Mapping from query string to desired data structure
+    query_to_response = {item['query']: item for item in flattened_queries}
+
+    # Prepare the results
+    results = []
+    for entry in detailed_responses:
+        query_text = entry['query']['query']
+        if query_text in query_to_response:
+            results.append({
+                'query': query_text,
+                'response': entry['response']['text'],
+                'exercise_format': entry['query']['requested_exercise_format'],
+                'topic': entry['query']['topic']
+            })
+    
+    # Save results to a new JSON file
+    write_json(results, output_file)
+    print(f"Results written to {output_file}")

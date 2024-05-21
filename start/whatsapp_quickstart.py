69c397180fe83b4c9c0edb83d62f69955f248d4b
Victor Oldensand <victormag24@gmail.com>
2024-05-21 08:36:32 +0200

    Misc: get whatsapp single messages to work

--- start/whatsapp_quickstart.py
+++ start/whatsapp_quickstart.py
@@ -14,7 +14,6 @@ ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 RECIPIENT_WAID = os.getenv("RECIPIENT_WAID")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
 VERSION = os.getenv("VERSION")
-
 APP_ID = os.getenv("APP_ID")
 APP_SECRET = os.getenv("APP_SECRET")
 

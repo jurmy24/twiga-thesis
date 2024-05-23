ec973092a177ac8d66a64184d09e5242306acaca
Victor Oldensand <victormag24@gmail.com>
2024-05-23 14:07:42 +0200

    Misc: create database utils file and add databases to the gitignore

--- app/utils/openai_utils.py
+++ app/utils/openai_utils.py
@@ -1,7 +1,5 @@
-import logging
 import os
-import shelve
-import time
+from typing import List
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -12,30 +10,7 @@ OPENAI_ASSISTANT_ID = os.getenv("OPENAI_ASSISTANT_ID")
 client = OpenAI(api_key=OPENAI_API_KEY)
 
 
-# Use context manager to ensure the shelf file is closed properly
-def check_if_thread_exists(wa_id):
-    with shelve.open("threads_db") as threads_shelf:
-        return threads_shelf.get(wa_id, None)
-
-
-def store_thread(wa_id, thread_id):
-    with shelve.open("threads_db", writeback=True) as threads_shelf:
-        threads_shelf[wa_id] = thread_id
-
-
-def clear_threads_db():
-    with shelve.open("threads_db", writeback=True) as threads_shelf:
-        threads_shelf.clear()
-
-
-def upload_file(path):
-    # Upload a file with an "assistants" purpose
-    file = client.files.create(
-        file=open("../../data/airbnb-faq.pdf", "rb"), purpose="assistants"
-    )
-
-
-def create_assistant(file):
+def create_assistant():
     """
     You currently cannot set the temperature for Assistant via the API.
     """
@@ -64,18 +39,6 @@ def create_assistant(file):
         ],
     )
     return assistant
-
-
-def inspect_threads_db():
-    with shelve.open("threads_db") as threads_shelf:
-        if len(threads_shelf) == 0:
-            print("The threads database is empty.")
-        else:
-            for key in threads_shelf:
-                print(f"Key: {key} -> Value: {threads_shelf[key]}")
-
-
-from typing import List
 
 
 # Define the Message and ContentBlock classes as per your data structure
@@ -152,12 +115,3 @@ def print_conversation(messages: SyncCursorPage):
                     if block.type == "text"
                 )
                 print(f"### {sender}: {content}")
-
-
-if __name__ == "__main__":
-    # Clear the database
-    clear_threads_db()
-    print("Threads database cleared.")
-
-    # Inspect the database
-    inspect_threads_db()

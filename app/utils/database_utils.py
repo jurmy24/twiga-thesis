3b7715fde0d09670554f4be75df611d367b97a85
Victor Oldensand <victormag24@gmail.com>
2024-05-23 14:24:01 +0200

    Misc: clean database_utils file to make the functions more generalizable to different databases

--- app/utils/database_utils.py
+++ app/utils/database_utils.py
@@ -1,65 +1,68 @@
 import os
 import shelve
-
-from dotenv import load_dotenv
-
-load_dotenv()
-SHELVE_FILENAME = os.getenv("SHELVE_FILENAME", "onboarding_db")
+from typing import Dict
 
 
-def clear_onboarding_db():
-    with shelve.open(SHELVE_FILENAME, writeback=True) as db:
+def clear_db(db_name: str):
+    with shelve.open(db_name, writeback=True) as db:
         db.clear()
 
 
-def reset_conversation(wa_id):
-    with shelve.open(SHELVE_FILENAME) as db:
+""" Users Database Functions """
+
+
+def reset_conversation(wa_id: str, db_name: str = "users"):
+    with shelve.open(db_name) as db:
         db[wa_id] = {"state": "start"}
 
 
-def get_user_state(wa_id):
-    with shelve.open(SHELVE_FILENAME) as db:
+def get_user_state(wa_id: str, db_name: str = "users"):
+    with shelve.open(db_name) as db:
         return db.get(wa_id, {"state": "start"})
 
 
-def update_user_state(wa_id, state_update):
-    with shelve.open(SHELVE_FILENAME) as db:
+def update_user_state(wa_id: str, state_update: Dict[str, str], db_name: str = "users"):
+    with shelve.open(db_name) as db:
         db[wa_id] = state_update
 
 
-# Use context manager to ensure the shelf file is closed properly
-def check_if_thread_exists(wa_id):
-    with shelve.open("threads_db") as threads_shelf:
-        return threads_shelf.get(wa_id, None)
+""" Threads Database Functions """
 
 
-def store_thread(wa_id, thread_id):
-    with shelve.open("threads_db", writeback=True) as threads_shelf:
-        threads_shelf[wa_id] = thread_id
+def check_if_thread_exists(wa_id: str, db_name: str = "threads"):
+    with shelve.open(db_name) as db:
+        return db.get(wa_id, None)
 
 
-def clear_threads_db():
-    with shelve.open("threads_db", writeback=True) as threads_shelf:
-        threads_shelf.clear()
+def store_thread(wa_id: str, thread_id: str, db_name: str = "threads"):
+    with shelve.open(db_name, writeback=True) as db:
+        db[wa_id] = thread_id
 
 
-def inspect_threads_db():
-    with shelve.open("threads_db") as threads_shelf:
-        if len(threads_shelf) == 0:
-            print("The threads database is empty.")
+def inspect_db(db_name: str):
+    with shelve.open(db_name) as db:
+        if len(db) == 0:
+            print(f"The {db_name} database is empty.")
         else:
-            for key in threads_shelf:
-                print(f"Key: {key} -> Value: {threads_shelf[key]}")
+            for key in db:
+                print(f"Key: {key} -> Value: {db[key]}")
 
 
 if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    USERS_DATABASE = os.getenv("USERS_DATABASE", "users")
+    THREADS_DATABASE = os.getenv("THREADS_DATABASE", "threads")
+
     # Clear the threads database
-    clear_threads_db()
+    print("Threads database.")
+    inspect_db(THREADS_DATABASE)
+    clear_db(THREADS_DATABASE)
     print("Threads database cleared.")
 
-    # Inspect the database
-    inspect_threads_db()
-
     # Clear the user-info database
-    clear_onboarding_db()
+    print("Users database.")
+    inspect_db(USERS_DATABASE)
+    clear_db(USERS_DATABASE)
     print("Onboarding database cleared.")
